a73b6cd438269e311b842a881fe0a075bc710aab
Rick Barrette <rickbarrette@gmail.com>
2026-02-20 19:48:55 -0500

    2026.2.4

--- init.rb
+++ init.rb
@@ -14,7 +14,7 @@ Redmine::Plugin.register :redmine_qbo_vehicles do
   name 'Redmine QBO Vehicles plugin'
   author 'Rick Barrette'
   description 'This is a plugin for Redmine to intergrate with the redmine_qbo plugin to provide vehicle data tracking'
-  version '2026.2.3'
+  version '2026.2.4'
   url 'https://github.com/rickbarrette/redmine_qbo_vehicles'
   author_url 'https://barrettefabrication.com'
   requires_redmine version_or_higher: '6.1.0'
